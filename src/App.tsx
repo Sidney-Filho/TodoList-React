99451b1e3ea4a112165b5086e797078203bc452b
Sidney-Filho <sidinhocostafilho@gmail.com>
2024-05-13 23:37:20 +0100

    add count for Tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import logo from './assets/Logo.svg'
 import plus from './assets/plus.svg'
@@ -5,22 +6,90 @@ import Task from './components/Task'
 
 function App() {
 
+  const [text, setText] = useState("")
+  const [tasks, setTasks] = useState<string[]>([])
+  const [createdTasks, setCreatedTasks] = useState<number>(0)
+  const [completedTasks, setCompletedTasks] = useState<number>(0)
+
+  function handleAddTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    if(!text.trim()) {
+      window.alert('Por favor, insira o texto da tarefa.')
+      return
+    }
+
+    const taskExists = tasks.some(task => task === text)
+
+    if(taskExists) {
+      window.alert("Esta tarefa já existe!")
+    } else {
+      setTasks(prevTasks => [...prevTasks, text])
+      setText("")
+      console.log(text)
+      setCreatedTasks(prevCreatedTasks => prevCreatedTasks + 1)
+    }
+  }
+
+  function handleDeleteTask(id: number) {
+    const updatedTasks = tasks.filter((_, taskIndex) => taskIndex !== id)
+    setTasks(updatedTasks)
+    setCreatedTasks(prevCreatedTasks => prevCreatedTasks - 1)
+  }
+
+  function handleCompleteTask(id: number, isComplete: boolean) {
+    setCompletedTasks(prevCompletedTasks => isComplete ? prevCompletedTasks + 1 : prevCompletedTasks - 1)
+  }
+
 
   return (
     <div>
-      <div className='bg-neutral-950 flex justify-center items-center h-56'>
-        <img src={logo} alt="Logo" className='pb-6' />
-      </div>
-        <div className='mt-[-25px] flex justify-center items-center w-full gap-3'>
-          <input type="text" className='border-none bg-neutral-800 text-white placeholder:text-gray300 p-3 rounded-md shadow-md w-3/5 focus-within:border-none' placeholder='Adicione uma nova tarefa'/>
-          <button className='p-3 text-white flex justify-center items-center gap-2 bg-blue-500 rounded-md'>
-            Criar
-            <img src={plus} alt="Icone Adicionar" />
-          </button>
-        </div>
-
-        <Task/>
+    <div className='bg-neutral-950 flex justify-center items-center h-56'>
+      <img src={logo} alt="Logo" className='pb-6' />
     </div>
+    <form onSubmit={handleAddTask} className='mt-[-25px] flex justify-center items-center w-full gap-3'>
+      <input
+        type="text"
+        className='border-none bg-neutral-800 text-white placeholder:text-gray300 p-3 rounded-md shadow-md w-3/5 focus-within:border-none'
+        placeholder='Adicione uma nova tarefa'
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button
+        type="submit"
+        className='p-3 text-white flex justify-center items-center gap-2 bg-blue-500 rounded-md'
+      >
+        Criar
+        <img src={plus} alt="Icone Adicionar" />
+      </button>
+    </form>
+
+    <div className="flex justify-around w-full mt-20">
+      <div className="flex gap-2 justify-center items-center">
+        <span className="rounded-xl bg-zinc-700 px-2 text-white">
+          {createdTasks}
+        </span>
+        <p className="text-blue-400">Tarefas Criadas</p>
+      </div>
+
+      <div className="flex gap-2 justify-center items-center">
+        <span className="rounded-xl bg-zinc-700 px-2 text-white">
+          {completedTasks}
+        </span>
+        <p className="text-purple-400">Concluídas</p>
+      </div>
+    </div>
+
+    {tasks.map((task, index) => (
+      <Task
+        key={index}
+        text={task}
+        onDeleteTask={() => handleDeleteTask(index)}
+        onCompleteTask={handleCompleteTask}
+        index={index}
+      />
+    )) }
+  </div>
   )
 }
 
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,12 +1,45 @@
-function Task() {
+import { useState } from 'react'
+import trash from '../assets/trash.svg'
+
+interface TaskProps {
+  text: string,
+  index: number,
+  onDeleteTask: (id: number) => void,
+  onCompleteTask: (id: number, isComplete: boolean) => void
+}
+
+function Task({text, index, onDeleteTask, onCompleteTask}: TaskProps) {
+
+  const [isChecked, setIsChecked] = useState(false)
+
+  function handleCheckboxChange() {
+    const newIsChecked = !isChecked
+    setIsChecked(newIsChecked)
+    onCompleteTask(index, newIsChecked)
+  }
+
   return(
-    <div className="flex justify-evenly">
-      <div>
-        <input type="radio" />
-        <p>Texto</p>
-      </div>
-      <div>
-        <button>Delete</button>
+    <div className="flex flex-col justify-center items-center w-full">
+      <div className="flex justify-between p-8 bg-neutral-600 items-center w-5/6 mt-10 rounded-md text-white">
+        <div className="flex items-center justify-center gap-4">
+          <input 
+            type="checkbox" 
+            className='checkbox" className="h-5 w-5 text-blue-500 border-gray-300 rounded-md focus:ring-blue-500 cursor-pointer' 
+            onChange={handleCheckboxChange}
+            checked={isChecked}
+          />
+          <p className='text-xl'>
+            {text}
+          </p>
+        </div>
+        <div>
+          <button onClick={() => onDeleteTask(index)} className='hover:text-red-600'>
+            <span className="text-white hover:text-red-600">
+              <img src={trash} alt="Deletar Icone" />
+            </span>
+          </button>
+
+        </div>
       </div>
     </div>
   )
