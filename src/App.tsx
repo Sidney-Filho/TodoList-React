736da72659b024c7179c574a727a2cb8b41bfeb3
Sidney-Filho <sidinhocostafilho@gmail.com>
2024-05-14 17:06:29 +0100

    finish app

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,60 @@ import logo from './assets/Logo.svg'
 import plus from './assets/plus.svg'
 import Task from './components/Task'
 
+interface Task {
+  text: string,
+  isComplete: boolean;
+}
+
 function App() {
 
   const [text, setText] = useState("")
-  const [tasks, setTasks] = useState<string[]>([])
+  const [tasks, setTasks] = useState<Task[]>([])
   const [createdTasks, setCreatedTasks] = useState<number>(0)
   const [completedTasks, setCompletedTasks] = useState<number>(0)
 
   function handleAddTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
+    // Verifica se o input está vazio
     if(!text.trim()) {
       window.alert('Por favor, insira o texto da tarefa.')
       return
     }
 
-    const taskExists = tasks.some(task => task === text)
+    // Percore o array [tasks] para ver se não há uma tarefa igual
+    const taskExists = tasks.some(task => task.text === text)
 
+    // Se a tarefa existe da um alerta, se não existe cria a tarefa
     if(taskExists) {
       window.alert("Esta tarefa já existe!")
     } else {
-      setTasks(prevTasks => [...prevTasks, text])
+
+      // Cria um novo array de tarefas com as tarefas antigas, e a nova tarefa
+      setTasks(prevTasks => [...prevTasks, {text, isComplete: false}])
       setText("")
-      console.log(text)
+      // Adiciona a contagem de tarefas criadas
       setCreatedTasks(prevCreatedTasks => prevCreatedTasks + 1)
     }
   }
 
   function handleDeleteTask(id: number) {
-    const updatedTasks = tasks.filter((_, taskIndex) => taskIndex !== id)
+    const taskToDelete = tasks[id];
+    if(taskToDelete.isComplete) {
+      setCompletedTasks(prevCompletedTasks => prevCompletedTasks - 1)
+    }
+
+    const updatedTasks = tasks.filter((_, taskIndex) => taskIndex !== id) 
     setTasks(updatedTasks)
     setCreatedTasks(prevCreatedTasks => prevCreatedTasks - 1)
   }
 
   function handleCompleteTask(id: number, isComplete: boolean) {
+    setTasks(prevTasks => 
+      prevTasks.map((task,index) => (
+        index === id ? {...task, isComplete} : task
+      ))
+    )
     setCompletedTasks(prevCompletedTasks => isComplete ? prevCompletedTasks + 1 : prevCompletedTasks - 1)
   }
 
@@ -50,14 +70,14 @@ function App() {
     <form onSubmit={handleAddTask} className='mt-[-25px] flex justify-center items-center w-full gap-3'>
       <input
         type="text"
-        className='border-none bg-neutral-800 text-white placeholder:text-gray300 p-3 rounded-md shadow-md w-3/5 focus-within:border-none'
+        className='border-none bg-neutral-800 text-white placeholder:text-gray300 p-3 rounded-md shadow-md w-2/4 focus-within:border-none'
         placeholder='Adicione uma nova tarefa'
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
       <button
         type="submit"
-        className='p-3 text-white flex justify-center items-center gap-2 bg-blue-500 rounded-md'
+        className='p-3 text-white flex justify-center items-center gap-2 bg-darkBlue hover:bg-blue100 rounded-md'
       >
         Criar
         <img src={plus} alt="Icone Adicionar" />
@@ -66,26 +86,33 @@ function App() {
 
     <div className="flex justify-around w-full mt-20">
       <div className="flex gap-2 justify-center items-center">
-        <span className="rounded-xl bg-zinc-700 px-2 text-white">
+        <p className="text-blue-400 font-bold text-md">Tarefas Criadas</p>
+        <span className="rounded-xl bg-zinc-700 px-2 py-1 text-white text-sm font-bold">
           {createdTasks}
         </span>
-        <p className="text-blue-400">Tarefas Criadas</p>
       </div>
 
       <div className="flex gap-2 justify-center items-center">
-        <span className="rounded-xl bg-zinc-700 px-2 text-white">
-          {completedTasks}
-        </span>
-        <p className="text-purple-400">Concluídas</p>
+        <p className="text-purple300 font-bold text-md">Concluídas</p>
+        {createdTasks > 0 ? (
+          <span className="rounded-xl bg-zinc-700 px-2 py-1 text-white text-sm font-bold">
+            {completedTasks} de {createdTasks}
+          </span>
+        ) : (
+          <span className='rounded-xl bg-zinc-700 px-2 py-1 text-white text-sm font-bold'>
+            {completedTasks}
+          </span>
+        )}
       </div>
     </div>
 
     {tasks.map((task, index) => (
       <Task
         key={index}
-        text={task}
+        text={task.text}
+        isComplete={task.isComplete}
         onDeleteTask={() => handleDeleteTask(index)}
-        onCompleteTask={handleCompleteTask}
+        onCompleteTask={(index:number, isComplete: boolean) => handleCompleteTask(index, isComplete)}
         index={index}
       />
     )) }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,16 @@ export default {
   theme: {
     extend: {
       colors: {
+        gray100: '#F2F2F2',
         gray300: '#808080',
+        gray400: '#333333',
+        gray500: '#262626',
+        gray600: '#1A1A1A',
+        red300: '#E25858',
+        purple300: '#8284FA',
+        blue100: '#4EA8DE',
+        darkPurple: '#5E60CE',
+        darkBlue: '#1E6F9F'
       }
     },
   },
